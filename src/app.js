d8f8e97ca4fc26a3b935b8a3b81ef68ac6a52996
divRobles <j.roblesmendoza96@gmail.com>
2022-05-11 14:22:55 +0000

    commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,21 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  let result = [];
+  const funcion = (pronoun, adj, noun) => {
+    for (let a = 0; a < pronoun.length; a++) {
+      for (let e = 0; e < adj.length; e++) {
+        for (let i = 0; i < noun.length; i++) {
+          result.push(`${pronoun[a]}${adj[e]}${noun[i]}.com`);
+        }
+      }
+    }
+  };
+
+  funcion(pronoun, adj, noun);
+  console;
 };
